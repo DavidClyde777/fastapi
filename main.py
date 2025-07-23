a95bdb8418643f08a2cc28018bdf8b31e527287a
DavidClyde777 <144747510+DavidClyde777@users.noreply.github.com>
2025-07-22 23:21:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI, UploadFile, File
 from PIL import Image
 from io import BytesIO
-from collections import Counter
 
 app = FastAPI()
 
-@app.post("/extract-hex")
-async def extract_hex(file: UploadFile = File(...)):
-    contents = await file.read()
-    image = Image.open(BytesIO(contents)).convert('RGB')
-    pixels = list(image.getdata())
+@app.post("/extract-colors")
+async def extract_colors(file: UploadFile = File(...)):
+    image_data = await file.read()
+    image = Image.open(BytesIO(image_data)).convert("RGB")
+    colors = image.getcolors(maxcolors=1000000)  # returns list of (count, color)
 
-    # Get the most common color
-    most_common_color = Counter(pixels).most_common(1)[0][0]
-    hex_color = '#{:02x}{:02x}{:02x}'.format(*most_common_color).upper()
+    if not colors:
+        return {"error": "Too many colors to process."}
 
-    return {"hex": hex_color}
+    hex_colors = list({f"#{r:02x}{g:02x}{b:02x}" for count, (r, g, b) in colors})
+    return {"hex_colors": hex_colors}
